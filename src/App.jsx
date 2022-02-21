8d97c3d3577872860f11f5d2c5aa9f3495a8850a
Nick Hennessy <njhnny@gmail.com>
2022-02-20 20:11:53 -0800

    change app to jsx

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React,{ Component } from "react";
+export class NavBar extends Component{
+  render() {
+    <h1>Hello</h1>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import "jquery";
 import "popper.js";
 import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { NavBar } from "./App";
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-var element =<div>HelloWorld</div>;
+var element =<button class="btn btn-danger">HelloWorld</button>;
 
 ReactDOM.render(
+  <NavBar />,
   element,
   // <React.StrictMode>
   //   <App />
