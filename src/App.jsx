8d23cab31824cb4f73a03578909874f4a980e831
Nick Hennessy <njhnny@gmail.com>
2022-02-27 15:35:14 -0800

    display nav bar with css applied

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ class NavBar extends Component{
   render() {
     return (
       <React.Fragment>
-    <h1>Hello</h1>;
-    <p>Body text</p>;
+    <h1>Hello</h1>
+    <p class="highlight">Body text</p>
     </React.Fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,21 +6,17 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./App";
 import './index.css';
-// import App from './App';
+//import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 var element =<button class="btn btn-danger">HelloWorld</button>;
 
 ReactDOM.render(
   <NavBar />,
-  element,
+  //element,
   // <React.StrictMode>
-  //   <App />
+//<App />,
   // </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
