07ca9d455182a55e02cf5eba75e508e98f165251
Nick Hennessy <njhnny@gmail.com>
2022-03-01 18:42:01 -0800

    app working with nested components

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+
+export default class App extends
+  Component {
+  render() {
+    return <NavBar />;
+  }
+}
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,51 +1,21 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-
 class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
-          <a className="navbar-brand" href="/#">
-            eCommerce
-          </a>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+        <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-style">
+          <a className="navbar-brand" href="/#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  Login
-                </Link>
+                <a className="nav-link" href="/#">Home</a>
               </li>
-
               <li className="nav-item">
-                <Link to="/dashboard" className="nav-link">
-                  Dashboard
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to="/customers" className="nav-link">
-                  Customers
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to="/cart" className="nav-link">
-                  Shopping Cart
-                </Link>
+                <a className="nav-link" href="/#">Link</a>
               </li>
             </ul>
           </div>
